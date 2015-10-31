306dd03eb6dda747afada0b2d13e1e9f90913fe6
CY Chiang <chiang831@gmail.com>
2015-10-31 22:02:11 +0800

    Use poster from IMDB

--- movie_trailer.py
+++ movie_trailer.py
@@ -10,15 +10,15 @@ movies = [
    Movie(
        title='The Intern',
        video_url='https://www.youtube.com/watch?v=ZU3Xban0Y6A',
-       image_url='http://www.impawards.com/2015/posters/intern_xlg.jpg'),
+       image_url='http://ia.media-imdb.com/images/M/MV5BMTUyNjE5NjI5OF5BMl5BanBnXkFtZTgwNzYzMzU3NjE@._V1_SX300.jpg'),
    Movie(
        title='The Martian',
        video_url='https://www.youtube.com/watch?v=ej3ioOneTy8',
-       image_url='https://maggielieu.files.wordpress.com/2015/10/maxresdefault.jpg'),
+       image_url='http://ia.media-imdb.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_SX300.jpg'),
    Movie(
        title='Batman v Superman Dawn of Justice',
        video_url='https://www.youtube.com/watch?v=T89tvJTCohA',
-       image_url='http://masterherald.com/wp-content/uploads/2015/06/Batman-v-Superman-Dawn-of-Justice.jpg')
+       image_url='http://ia.media-imdb.com/images/M/MV5BMTUyMTIxNzE1M15BMl5BanBnXkFtZTgwOTU0ODQ4MTE@._V1_SX300.jpg')
 ]
 
 fresh_tomatoes.open_movies_page(movies)
